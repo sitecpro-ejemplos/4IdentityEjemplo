00d71999b18125af28479a9186ba08453a177367
osanchez <osanchez@sitecpro.com>
2021-09-03 00:28:54 -0600

    Initial commit

---
+++ Api4identity/Api4identity/Controllers/archivo.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api4identity.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class archivo : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult get()
+        {
+            return Ok();
+        }
+
+        [HttpPost("subir")]
+        public IActionResult subirArchivo([FromForm] string documentID, IFormFile attach)
+        {
+            // _logger.LogInformation("En UploadSignedFile4Identity, id = " + documentID);
+            if (attach == null)
+                return Ok();
+            if (attach.Length == 0)
+                return Ok();
+            try
+            {
+                string nombre = Guid.NewGuid().ToString("N") + ".pdf";
+                using (var stream = new FileStream(nombre, FileMode.Create))
+                    attach.CopyTo(stream);
+
+                if (!System.IO.File.Exists(nombre))
+                    throw new Exception("Error inesperado, el archivo firmado por 4Identity no existe");
+
+                return Redirect("http://localhost/4identity/exito.html");
+            }
+            catch (Exception)
+            {
+                return Redirect("http://localhost/4identity/fracaso.html");
+            }
+        }
+    }
+}
